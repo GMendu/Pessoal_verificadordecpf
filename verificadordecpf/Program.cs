8f61e343a01b24894a2161f12f5653edab75a8a0
Gabriel Mendonça <bielm2000@hotmail.com>
2021-08-05 17:14:01 -0300

    Adicionar arquivos de projeto.

---
+++ verificadordecpf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace verificadordecpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Verificador a = new Verificador();
+            Console.Write("digite o cpf(apenas números): ");
+            string cepefe = Console.ReadLine();
+            a.Verifica(cepefe);
+        }
+    }
+}
---
+++ verificadordecpf/Verificador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace verificadordecpf
+{
+    class Verificador
+    {
+        public int Verificador1 { get; set; }
+        public int Verificador2 { get; set; }
+
+        int j = 10;
+        public void Verifica(string cpf)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Verificador1 += int.Parse(cpf[i].ToString()) * j;
+                Console.WriteLine(Verificador1);
+                j--;
+            }
+            Verificador1 = (Verificador1 * 10) % 11;
+            Console.WriteLine(Verificador1);
+            if (Verificador1 == int.Parse(cpf[9].ToString()))
+            {
+                j = 11;
+                for (int i = 0; i<10;i++)
+                {
+                    Verificador2 += int.Parse(cpf[i].ToString()) * j;
+                    Console.WriteLine(Verificador2);
+                    j--;
+                }
+                Verificador2 = (Verificador2 * 10) % 11;
+                Console.WriteLine(Verificador2);
+                if (Verificador2 == int.Parse(cpf[10].ToString()))
+                {
+                    Console.WriteLine("CPF Válido");
+                }
+                else
+                {
+                    Console.WriteLine("CPF Inválido");
+                }
+            }
+            else
+            {
+                Console.WriteLine("CPF inválido");
+            }
+        }
+    }
+}
